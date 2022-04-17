8cb3fe2b59d6b5870a61c4944a8fa3e731c7ef07
burak gün <burak.gun@hotmail.com>
2022-04-17 23:02:45 +0300

    json files added

---
+++ JSON_Example.py
@@ -0,0 +1,44 @@
+# 17.04.2022
+# Burak GÜN 
+# burak.gun@hotmail.com
+
+"""
+import json
+
+# Creating a dictionary
+Dictionary ={1:'Welcome', 2:'to',3:'Geeks', 4:'for',5:'Geeks'}
+
+# Converts input dictionary into
+# string and stores it in json_string
+json_string = json.dumps(Dictionary)
+print('Equivalent json string of input dictionary:',json_string)
+print("	 ")
+
+# Checking type of object
+# returned by json.dumps
+print(type(json_string))
+
+"""
+"""
+import json
+
+f = open('screen.json',)
+
+data = json.load(f)
+
+data_x = data['widget']
+
+for i in data_x['text']:
+    print(i)
+
+"""
+
+import json
+
+file = open('Books.json',)
+
+data = json.load(file)
+
+data2 = data['books']
+for i in data2:
+    print(i['title'])
