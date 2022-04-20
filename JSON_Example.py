61940535dfb156408b9652b48bf1fd6eb2a4d748
burak gün <burak.gun@hotmail.com>
2022-04-21 00:50:35 +0300

    json examples

--- JSON_Example.py
+++ JSON_Example.py
@@ -34,6 +34,8 @@ for i in data_x['text']:
     print(i)
 
 """
+# The json.load() is used to read the JSON document from file.
+# The json.loads() is used to convert the JSON String document into the Python dictionary.
 
 import json
 from prettytable import PrettyTable
@@ -42,25 +44,40 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.field_names = ['Title', 'Price', 'Empty']
 
-file = open('Books.json',)
-data = json.load(file)
+# with open ile açıldığı zaman dosyayı kapatmamıza gerek yok
+# Sistem otomatik olarak kapatır.
+with open('Books.json') as file:
+    data1 = json.load(file)
+
+print((data1['books']))
+print(type(data1['books']))
+
+# print((data1['books'][0]['language'][0:3])) 
+#for x in range( len(data1['books']) ):
+print((data1['books'][x]['title'])) 
 
 
-widget_file = open('screen.json')
-data_widget = json.load(widget_file,)
 
-print(type(data_widget['widget']), ' ' , type(data['books']))
-
-# load  -- json file
-# loads -- string
+# print(data1["books"][0:2])
+# print(type(data1["books"][1]))
+# print(type(data2))
 
 
+# widget_file = open('screen.json')
+# data_widget = json.load(widget_file,)
 
-data2 = data['books']
-for i in data2:
-    table.add_row( [ i['title'], i['price'] ,type(data) ]  )
+# print(type(data_widget['widget']), ' ' , type(data['books']))
 
-print(table)
-    #print(i['title'], i['price'])
+# # load  -- json file
+# # loads -- string
+
+
+
+# data2 = data['books']
+# for i in data2:
+#     table.add_row( [ i['title'], i['price'] ,type(data) ]  )
+
+# print(table)
+#     #print(i['title'], i['price'])
 
 
